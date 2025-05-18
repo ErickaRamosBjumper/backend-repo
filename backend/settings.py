c7372ef72ee6b8b7ee629cd839a113644c2bea65
Ericka Ramos <107136170+ErickaRamosBjumper@users.noreply.github.com>
2025-05-18 22:21:58 +0200

    update bck

--- backend/settings.py
+++ backend/settings.py
@@ -17,7 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
+    'drf_yasg',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -84,4 +85,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
+}
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Basic': {
+            'type': 'basic'
+        }
+    },
+    'OPERATIONS_SORTER': 'alpha',
+    'VALIDATOR_URL': None,
 }
